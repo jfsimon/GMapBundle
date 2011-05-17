f59677ae50b41185e7e1ab99f8753290119381ea
Geoff Ford <g_ford@hotmail.com>
2011-05-17 23:30:26 +1000

    Updated for use with PR12.

--- DependencyInjection/GMapExtension.php
+++ DependencyInjection/GMapExtension.php
@@ -2,18 +2,19 @@
 
 namespace Bundle\GMapBundle\DependencyInjection;
 
-use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\Config\FileLocator;
 
-class GMapExtension extends Extension
+class GMapExtension implements ExtensionInterface
 {
 
-    public function configLoad($config, ContainerBuilder $container)
+    public function load(array $config, ContainerBuilder $container)
     {
         // load definition
-        if (!$container->hasDefinition('gmap')) {
-            $loader = new XmlFileLoader($container, __DIR__.'/../Resources/config');
+        if (!$container->hasDefinition('g_map')) {
+            $loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
             $loader->load('gmap.xml');
         }
 
@@ -21,8 +22,8 @@ class GMapExtension extends Extension
         foreach(array('polyline_encoder', 'geocoder', 'elevation') as $service) {
             if(isset($config[$service]) && is_array($config[$service])) {
                 $container->setParameter(
-                    'gmap.'.$service.'.options',
-                    array_replace($container->getParameter('gmap.'.$service.'.options'), $config[$service])
+                    'g_map.'.$service.'.options',
+                    array_replace($container->getParameter('g_map.'.$service.'.options'), $config[$service])
                 );
             }
         }
@@ -40,6 +41,6 @@ class GMapExtension extends Extension
 
     public function getAlias()
     {
-        return 'gmap';
+        return 'g_map';
     }
 }
